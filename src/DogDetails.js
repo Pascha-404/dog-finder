06bb1169117d09c48703d4ed40c04e90bedcda74
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-18 17:59:06 +0200

    starter code

---
+++ src/DogDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DogDetails extends Component{
+    render() {
+        return (
+            <div className="DogDetails">
+
+            </div>
+        )
+    }
+}
+
+export default DogDetails;
