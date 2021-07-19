d3d897286120a1be8bd548c89ee405fab1cf6972
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-19 16:59:06 +0200

    replaced state with props

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-class DogDetails extends Component{
+class DogDetails extends Component {
     render() {
-        return (
-            <div className="DogDetails">
-
-            </div>
-        )
-    }
+		return (
+			<div className='DogDetails card w-25'>
+				<img src={this.props.dog.src} className='card-img-top' alt={this.props.dog.name} />
+				<div className='card-body'>
+					<h5 className='card-title'>{this.props.dog.name}</h5>
+					<p className='card-text'>is {this.props.dog.age} years old</p>
+					<ul className='list-group list-group-flush'>
+						{this.props.dog.facts.map((f,idx) => (
+							<li key={idx} className='list-group-item'>{f}</li>
+						))}
+					</ul>
+					{/* <a href='#' className='btn btn-primary'>
+						Go somewhere
+					</a> */}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default DogDetails;
