5546cb6f15c5555e76301d4d05c2c2d3a6a115f9
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-19 16:58:49 +0200

    added method to get the right dog

--- src/Routes.js
+++ src/Routes.js
@@ -5,18 +5,21 @@ import DogDetails from './DogDetails';
 
 class Routes extends Component{
     render() {
+		const getDog = props => {
+			const name = props.match.params.name
+			const currentDog = this.props.dogs.find(d => d.name.toLowerCase() === name.toLowerCase())
+			return <DogDetails dog={currentDog} />
+		}
         return (
 					<Switch>
 						<Route
 							exact
 							path='/'
-							render={() => <DogsList dogs={this.props.dogs} />}></Route>
+							render={(routeProps) => <DogsList dogs={this.props.dogs} {...routeProps} />}></Route>
 						<Route
-							exact
-							path='/dogs/:name'
-							render={routeProps => (
-								<DogDetails {...routeProps} dogs={this.props.dogs} />
-							)}></Route>
+					exact
+					path='/dogs/:name'
+					render={getDog}></Route>
 						<Route render={() => <h1>Something went wrong!</h1>}></Route>
 					</Switch>
 				);
