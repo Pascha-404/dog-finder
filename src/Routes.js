7cb498af91a1e0ecff2c04df763a156ce1960eaa
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-21 17:08:37 +0200

    formated code

--- src/Routes.js
+++ src/Routes.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
-import { Switch, Route } from "react-router-dom"
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import DogsList from './DogsList';
 import DogDetails from './DogDetails';
 
-class Routes extends Component{
-    render() {
+class Routes extends Component {
+	render() {
 		const getDog = props => {
-			const name = props.match.params.name
-			const currentDog = this.props.dogs.find(d => d.name.toLowerCase() === name.toLowerCase())
-			return <DogDetails dog={currentDog} />
-		}
-        return (
-					<Switch>
-						<Route
-							exact
-							path='/'
-							render={(routeProps) => <DogsList dogs={this.props.dogs} {...routeProps} />}></Route>
-						<Route
+			const name = props.match.params.name;
+			const currentDog = this.props.dogs.find(
+				d => d.name.toLowerCase() === name.toLowerCase()
+			);
+			return <DogDetails dog={currentDog} />;
+		};
+		return (
+			<Switch>
+				<Route
 					exact
-					path='/dogs/:name'
-					render={getDog}></Route>
-						<Route render={() => <h1>Something went wrong!</h1>}></Route>
-					</Switch>
-				);
-    }
+					path='/'
+					render={routeProps => (
+						<DogsList dogs={this.props.dogs} {...routeProps} />
+					)}></Route>
+				<Route exact path='/dogs/:name' render={getDog}></Route>
+				<Route render={() => <h1>Something went wrong!</h1>}></Route>
+			</Switch>
+		);
+	}
 }
 
 export default Routes;
